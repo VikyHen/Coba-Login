bb011d57a3b3de3e82ca2c112ed0f66bc31f9a0e
VikyHen <vikyhendriko03@mhs.mdp.ac.id>
2023-03-01 09:26:19 +0700

    Commit ke2

--- app/src/main/java/com/if4a/cobalogin/Login_Activity.java
+++ app/src/main/java/com/if4a/cobalogin/Login_Activity.java
@@ -2,9 +2,12 @@ package com.if4a.cobalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -23,8 +26,39 @@ public class Login_Activity extends AppCompatActivity {
         etUsername = findViewById(R.id.et_username);
         etPassword = findViewById(R.id.et_password);
         btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = etUsername.getText().toString();
+                password = etPassword.getText().toString();
+
+                if(username.trim().equals(null)){
+                    etUsername.setError("Username tidak boleh kosong");
+                }
+                else if (password.trim().equals(null)){
+                    etPassword.setError("Password tidak boleh kosong");
+                }
+                else{
+                    if(username.equals("vikyhendriko") && password.equals("kamunanyae?")){
+                        KL.setPref(Login_Activity.this, MainActivity.keySPusername, username);
+                        KL.setPref(Login_Activity.this, MainActivity.keySPname, "Viky Hendriko");
+                        startActivity(new Intent(Login_Activity.this, MainActivity.class));
+                        finish();
+                    }
+                    if(username.equals("rickykaka") && password.equals("terserah")){
+                        KL.setPref(Login_Activity.this, MainActivity.keySPusername, username);
+                        KL.setPref(Login_Activity.this, MainActivity.keySPname, "Ricardo Kaka");
+                        startActivity(new Intent(Login_Activity.this, MainActivity.class));
+                        finish();
+                    }
+                    else{
+                        Toast.makeText(Login_Activity.this, "Username atau Password Belum Tepat", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
-
-
 
 }
--- app/src/main/java/com/if4a/cobalogin/MainActivity.java
+++ app/src/main/java/com/if4a/cobalogin/MainActivity.java
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvSesi;
+    private TextView tvSesi, tvName;
     private MaterialButton btnLogout;
     KendaliLogin KL = new KendaliLogin();
-    public static String keySPusername = "spPadang";
+    public static String keySPusername = "GtPJAx45LMjkuICbExYElQ==_username";
+    public static String keySPname = "GtPJAx45LMjkuICbExYElQ==_name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,21 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
 
             tvSesi = findViewById(R.id.tv_sesi);
+            tvName = findViewById(R.id.tv_name);
             btnLogout = findViewById(R.id.btn_logout);
+
+            tvSesi.setText(KL.getPref(MainActivity.this, keySPusername));
+            tvName.setText(KL.getPref(MainActivity.this, keySPname));
+
+            btnLogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    KL.setPref(MainActivity.this, keySPusername, null);
+                    KL.setPref(MainActivity.this, keySPname, null);
+                    startActivity(new Intent(MainActivity.this, Login_Activity.class));
+                    finish();
+                }
+            });
         }
         else{
             startActivity(new Intent(MainActivity.this, Login_Activity.class));
